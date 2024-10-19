df523abfc2af3897f78bbbb8e3cfb89886851530
Лоскутов Прохор <loskutovp333@gmail.com>
2024-10-19 15:55:45 +0300

    Address sanitizer test №9.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 
-int main() {
+int main() 
+{
 	std::vector<int> a(10);
-	for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) 
+	{
 		a[i] = i;
 	}
 	return 0;
